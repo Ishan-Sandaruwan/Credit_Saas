6528eb85fc3b885c1562ec30a896354652e666cd
Ishan-Sandaruwan <ishan7official@gmail.com>
2025-06-12 19:48:31 +0530

    create user completed

--- actions/auth/register.ts
+++ actions/auth/register.ts
@@ -1,11 +1,30 @@
 "use server";
+import db from "@/lib/db";
 import { RegisterSchema } from "@/schemas/auth";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: "Invalid Fields!" };
   }
-  return { success: "User account created!" };
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({ data: { name, email, password: hashedPassword } });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
